de354ed9fb304d2e9170544ceca53cdd5ed68eba
Ashlab <ashlabcodes05@gmail.com>
2026-01-06 23:53:41 +0530

    Day 1 Completed

---
+++ day 1/day1.py
@@ -0,0 +1,17 @@
+print("Hello world!")
+
+
+print("Hello world!" + "\n" + "Hello world!" + "\n" + "Hello world!")
+
+print("My name is" + " " + "Angela")
+
+name = input("Please enter your name: ")
+print(f"Hello {name}!")
+print(len(name))
+
+# Brand name Generator
+print("Welcome to band name generator")
+city = input("Please enter your city: ")
+pet_name = input("Please enter your pet name: ")
+
+print("Your band name is " + city + " " + pet_name)
